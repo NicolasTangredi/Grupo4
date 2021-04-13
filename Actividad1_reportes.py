065699e1c2686370b5a7035bfe3abc1ecfb2a649
Tobias Ezequiel Maler <tobimaler21@gmail.com>
2021-04-12 21:21:39 -0300

    Subo los reportes terminados

--- Actividad1_reportes.py
+++ Actividad1_reportes.py
@@ -10,18 +10,7 @@ def reportes (datos):
         Luego pide que se ingrese un rango sobre el cual operar e informar que alumnos
         se encuentran en ese rango """
     
-    print ('-' * 52)
-    print ('Elija sobre que notas quiere el reporte: \n')
-    print ("1 : sobre eval1")
-    print ("2 : sobre eval2")
-    print ("3 : sobre suma de ambas notas")
-    print ('-' * 52)
-    choice =  int(input ())
-    print ('-' * 52)
-    min = int(input('ingrese el valor minimo del rango con el que quiere operar \n'))
-    max = int(input('ingrese el valor minimo del rango con el que quiere operar \n'))
     
-
     def rep_eval1(datos, min, max):
         """ Recibe un diccionario que que tiene como claves los nombres de los alumnos
             y el valor minimo y maximo del rango. Si la nota de la eval1 se encuentra dentro de ese rango 
@@ -42,37 +31,47 @@ def reportes (datos):
         """ Recibe un diccionario que que tiene como claves los nombres de los alumnos
             y el valor minimo y maximo del rango. Si la nota de la suma de notas se encuentra dentro de ese rango 
             imprime el nombre del alumno """
+        b = True    
         for n in datos:
-            if datos[n][2] >= min and datos[n][2] <= max:
-                print  (n)
+            if datos[n][2] != -1:
+                if datos[n][2] >= min and datos[n][2] <= max:
+                     print  (n)
+            elif b:              
+                print ('Aun no se ha calculado la suma de la notas')
+                b =False
+
+                 
+    print ('-' * 52)
+    print ('Elija sobre que notas quiere el reporte: \n')
+    print ("1 : sobre eval1")
+    print ("2 : sobre eval2")
+    print ("3 : sobre suma de ambas notas")
+    print ('-' * 52)
+    choice =  int(input ())
+    print ('-' * 52)
     
     corte = True
     print ('-' * 52)
-    print ('Alumnos que tienen sus nota entre el rango ingresado:')
     while corte:
         if (choice == 1):
+            min = int(input('ingrese el valor minimo del rango con el que quiere operar \n'))
+            max = int(input('ingrese el valor maximo del rango con el que quiere operar \n'))
+            print ('Alumnos que tienen sus nota entre el rango ingresado:')
             rep_eval1(datos,min,max)
             corte = False
         elif (choice == 2):
+            min = int(input('ingrese el valor minimo del rango con el que quiere operar \n'))
+            max = int(input('ingrese el valor maximo del rango con el que quiere operar \n'))
+            print ('Alumnos que tienen sus nota entre el rango ingresado:')
             rep_eval2(datos,min,max)
             corte = False
         elif (choice == 3):
+            min = int(input('ingrese el valor minimo del rango con el que quiere operar \n'))
+            max = int(input('ingrese el valor maximo del rango con el que quiere operar \n'))
+            print ('Alumnos que tienen sus nota entre el rango ingresado:')
             rep_suma_tot(datos,min,max)
             corte = False
         else:
             print ('El valor ingresado no es valido, por favor vuelva a ingresar un valor valido')
-            choice =  input ()
+            choice =  int(input ())
     print ('-' * 52)
-
-
-
-
-
-
-
-
-
-
-
-
-    

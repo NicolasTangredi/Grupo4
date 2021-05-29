799863bd173f784e5bfe66be666989627cc297eb
Tomas Guardo <guardotomas02@gmail.com>
2021-05-29 17:37:19 -0300

    cambio los criterios de datos para que usen imagenes

--- Source/Handlers/crear_datos.py
+++ Source/Handlers/crear_datos.py
@@ -1,4 +1,5 @@
 import csv, json
+from posixpath import split
 
 def crear(semana):
     ''' crea un archivo con los datos a usar y un string con el criterio usado
@@ -7,7 +8,7 @@ def crear(semana):
     '''
 
     # abre los dos datasets
-    with open('data/netflix_titles.csv', 'r', encoding='utf-8') as data, open('data/summer.csv', 'r', encoding='utf-8') as data2:
+    with open('data/netflix_titles.csv', 'r', encoding='utf-8') as data, open('data/appstore_games.csv', 'r', encoding='utf-8') as data2:
         
         # criterios y datos a utilizar
         diccionario = {}
@@ -19,11 +20,11 @@ def crear(semana):
             crit = criterios[i]
 
             diccionario[dia] = {
-                'mañana': {
+                'imagenes': {
                     'criterio': crit[0][1],
                     'data': crit[0][0](full_data),
                 },
-                'tarde': {
+                'palabras': {
                     'criterio': crit[1][1],
                     'data': crit[1][0](full_data),
                 }
@@ -41,52 +42,55 @@ def definir_criterios():
     ''' Los criterios para elegir los datos y una descripcion.   
         Se organiza como:   
           
-        criterios(set) 
-            --- dia(set) 
-                --- mañana y tarde(tupla)
+        criterios(tupla) 
+            --- dia(tupla) 
+                --- imagenes o texto(tupla)
     '''
+
+    ""
+    
 
     # en general, se filtra a los datos por un criterio definido y
     # se mapea el filterObject, quedandose con el dato que se necesite,
     # para despues castearlo el map a lista
     criterios = [
-        (   # lunes por la mañana
-            (lambda data: list(map( lambda item: item['Athlete'], filter(lambda row: row['Discipline'] == 'Swimming' and row['Medal'] == 'Gold', data[0])))[:20]
-            , 'Atletas olimpicos de la disciplina de natacion con medallas de oro'), 
+        (   # lunes con imagenes
+            (lambda data: list(map( lambda item: item['Icon URL'], filter(lambda row: int(row['Original Release Date'].split('/')[2]) > 2000, data[0])))[:20]
+            , 'Juegos de la app store creados luego del año 2000'), 
             
-            # lunes por la tarde
+            # lunes con texto
             (lambda data: list(map( lambda item: item['title'], filter(lambda row: row['type'] == 'TV Show' and int(row['duration'].split(' ')[0]) > 2, data[1])))[:20]
             , 'Series de netflix con mas de 2 temporadas'), 
-        ),
-        (   # martes por la mañana
-            (lambda data: list(map( lambda item: item['Athlete'], filter(lambda row: row['Discipline'] == 'Fencing' and row['Medal'] == 'Silver', data[0])))[:20]
-            , 'Atletas olimpicos de la disciplina de esgrima con medallas de plata'),
+        ), 
+        (   # martes con imagenes
+            (lambda data: list(map( lambda item: item['Icon URL'], filter(lambda row: row['ID'][-1] == '1' and int(0 if not row['User Rating Count'] else row['User Rating Count']) > 400, data[0])))[:20]
+            , 'Juegos de la app store con mas de 400 reseñas y un id que termina en uno'),
             
-            # martes por la tarde
-            (lambda data: list(map( lambda item: item['title'], filter(lambda row: row['type'] == 'Movie' and 'Dramas' in row['listed_in'] and int(row['duration'].split(' ')[0]) > 60, data[1])))[:20]
+            # martes con texto
+            (lambda data: list(map( lambda item: item['title'], filter(lambda row: row['type'] == 'Movie' and 'Horror Movies' in row['listed_in'] and int(row['duration'].split(' ')[0]) > 60, data[1])))[:20]
             , 'Peliculas de terror en netflix con mas de 60 minutos')
         ),
-        (   # miercoles por la mañana
-            (lambda data: list(map( lambda item: item['Athlete'], filter(lambda row: row['Year'] == '2008' and row['Gender'] == 'Women', data[0])))[:20]
-            , 'Atletas olimpicos que participaron en las olimpiadas del 2008 y son mujeres'), 
+        (   # miercoles con imagenes
+            (lambda data: list(map( lambda item: item['Icon URL'], filter(lambda row: len(row['Languages'].split(',')) > 5 and int(row['Current Version Release Date'].split('/')[-1]) > 2018, data[0])))[:20]
+            , 'Los juegos de la app store con mas de 20 años de diferencia entre versiones'), 
             
-            # miercoles por la tarde
+            # miercoles con texto
             (lambda data: list(map( lambda item: item['title'], filter(lambda row: row['type'] == 'TV Show' and 1980 < int(row['release_year']) > 2000, data[1])))[:20]
             , 'Peliculas de netflix creadas entre los años 1980 y 2000')
-        ),
-        (   # jueves por la mañana
-            (lambda data: list(map( lambda item: item['Athlete'], filter(lambda row: row['City'] == 'Athens' and row['Sport'] == 'Gymnastics', data[0])))[:20]
-            , 'Gimnastas olimpicos que participaron en las olimpiadas en Atenas'), 
+        ), 
+        (   # jueves con imagenes
+            (lambda data: list(map( lambda item: item['Icon URL'], filter(lambda row: float(0 if row['Price'] in ['Price', ''] else row['Price']) > 2 and 'Puzzle' in row['Genres'], data[0])))[:20]
+            , 'Juegos del genero puzzle con un precio mayor a 2'), 
             
-            # jueves por la tarde
+            # jueves con texto
             (lambda data: list(map( lambda item: item['title'], filter(lambda row: row['type'] == 'TV Show' and 'Dramas' in row['listed_in'], data[1])))[:20]
             , 'Series de netflix que aparecen en la categoria de dramas')
         ),
-        (   # viernes por la mañana
-            (lambda data: list(map( lambda item: item['Athlete'], filter(lambda row: row['Country'] == 'USA' and row['Athlete'][0].lower() in 'cl', data[0])))[:20]
-            , 'Atletas olimpicos de Estados Unidos cuyo apellido comienza con una c o una l '), 
+        (   # viernes con imagenes
+            (lambda data: list(map( lambda item: item['Icon URL'], filter(lambda row: 'FR' in row['Languages'] and row['Name'][0].lower() == 'r', data[0])))[:20]
+            , 'Juego de la app store con lenguaje fances que su nombre empieza con r'), 
             
-            # viernes por la tarde
+            # viernes con texto
             (lambda data: list(map( lambda item: item['title'], filter(lambda row: 'robot' in row['description'].lower(), data[1])))[:20]
             , 'Peliculas o series de netflix cuya descripcion contiene la palabra robot')
         )
--- Source/Handlers/elegir_datos.py
+++ Source/Handlers/elegir_datos.py
@@ -1,7 +1,7 @@
 import json, datetime, random
 from ..Handlers import crear_datos
 
-def elegir_criterio():
+def elegir_criterio( tipo_dato ):
     ''' devuelve un diccionario que contiene una lista de strings, con un criterio que
         cambia dependiendo del dia de la semana, y un string con una 
         descripcion del criterio usado.
@@ -32,22 +32,14 @@ def elegir_criterio():
             print('Parece que hay problemas con los datos')
 
     if data != None:
-        # obtiene el dia y la hora
+        # obtiene el dia actual
         dia = datetime.datetime.now().weekday()
-        horario = datetime.datetime.now().hour
 
-        # semana actual y si es mañana o tarde
         # los fines de semana es una criterio aleatorio
         try:
             dia = semana[dia]
         except:
             dia = semana[random.randint(0,4)]
-        horario = horario // 12
 
-        # elije los datos por dia y franja horaria
-        if horario == 0:
-            print(dia, ' por la mañana')
-            return data[dia]['mañana']
-        else:
-            print(dia, ' por la tarde')
-            return data[dia]['tarde']
+        print('hoy es ', dia, ', estas jugando con: ', tipo_dato)
+        return data[dia][tipo_dato]

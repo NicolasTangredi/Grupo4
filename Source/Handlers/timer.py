bc401875e811e8b71d920d87b7163c5c78026a0a
NicolasTangredi <80898449+NicolasTangredi@users.noreply.github.com>
2021-06-23 17:16:27 -0300

    no se que toque

--- Source/Handlers/timer.py
+++
@@ -1,8 +0,0 @@
-import time as t
-
-def actualizar(start_timer):
-    """Devuelve un string con el tiempo transcurrido"""
-    tiempo = t.time() - start_timer
-    timer = 'Tiempo:' f'{round(tiempo // 60):02d}:{round(tiempo % 60):02d}'
-
-    return timer

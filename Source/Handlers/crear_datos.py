8fbd5feef0ed5ef70619b6e5a7298edc9ea8d6da
Tomas Guardo <guardotomas02@gmail.com>
2021-06-28 21:36:22 -0300

    agrego criterios por horario

--- Source/Handlers/crear_datos.py
+++ Source/Handlers/crear_datos.py
@@ -46,43 +46,43 @@ def definir_criterios(pal, img):
 
     return [
         (   # ----- lunes con imagenes -----
-            (dataframe_img[dataframe_img['Original Release Date'] >= '1/1/2001']['Icon URL'][:20],
+            (dataframe_img[dataframe_img['Original Release Date'] >= '1/1/2001']['Icon URL'][:40],
             'Juegos de la app store creados luego del año 2000'), 
             
             # ----- lunes con texto -----
-            (dataframe_pal[(dataframe_pal['type'] == 'TV Show') & (dataframe_pal['duration'] > '2')]['title'][:20], 
+            (dataframe_pal[(dataframe_pal['type'] == 'TV Show') & (dataframe_pal['duration'] > '2')]['title'][:40], 
             'Series de netflix con mas de 2 temporadas') 
         ), 
         (   # ----- martes con imagenes -----
-            (dataframe_img[(dataframe_img['ID'].apply(str).str[-1:] == '1') & (dataframe_img['User Rating Count'] > 400)]['Icon URL'][:20], 
+            (dataframe_img[(dataframe_img['ID'].apply(str).str[-1:] == '1') & (dataframe_img['User Rating Count'] > 400)]['Icon URL'][:40], 
             'Juegos de la app store con mas de 400 reseñas y un id que termina en uno'),
             
             # ----- martes con texto -----
-            (dataframe_pal[(dataframe_pal['type'] == 'Movie') & (dataframe_pal['duration'] > '60')]['title'][:20], 
+            (dataframe_pal[(dataframe_pal['type'] == 'Movie') & (dataframe_pal['duration'] > '60')]['title'][:40], 
             'Peliculas de netflix con mas de 60 minutos')
         ),
         (   # ----- miercoles con imagenes -----
-            (dataframe_img[(dataframe_img['Languages'].str.split(',').str.len() > 5) & (dataframe_img['Current Version Release Date'] >= '1/1/2019')]['Icon URL'][:20], 
+            (dataframe_img[(dataframe_img['Languages'].str.split(',').str.len() > 5) & (dataframe_img['Current Version Release Date'] >= '1/1/2019')]['Icon URL'][:40], 
             'Los juegos de la app store con mas de 20 años de diferencia entre versiones'), 
             
             # ----- miercoles con texto -----
-            (dataframe_pal[(dataframe_pal['type'] == 'TV Show') & ((1980 < dataframe_pal['release_year']) & (dataframe_pal['release_year'] > 2000))]['title'][:20], 
+            (dataframe_pal[(dataframe_pal['type'] == 'TV Show') & ((1980 < dataframe_pal['release_year']) & (dataframe_pal['release_year'] > 2000))]['title'][:40], 
             'Peliculas de netflix creadas entre los años 1980 y 2000')
         ), 
         (   # ----- jueves con imagenes -----
-            (dataframe_img[(dataframe_img['Price'] > 2) & (dataframe_img['Primary Genre'] == "Games")]['Icon URL'][:20],
+            (dataframe_img[(dataframe_img['Price'] > 2) & (dataframe_img['Primary Genre'] == "Games")]['Icon URL'][:40],
             'Juegos con un precio mayor a 2'), 
             
             # ----- jueves con texto -----
-            (dataframe_pal[(dataframe_pal['type'] == 'TV Show') & (['Dramas' in data for data in list(dataframe_pal['listed_in'])])]['title'][:20],
+            (dataframe_pal[(dataframe_pal['type'] == 'TV Show') & (['Dramas' in data for data in list(dataframe_pal['listed_in'])])]['title'][:40],
             'Series de netflix que aparecen en la categoria de dramas')
         ),
         (   # ----- viernes con imagenes -----
-            (dataframe_img[(['FR' in data for data in list(dataframe_img['Languages'].apply(str))]) & (dataframe_img['Name'].str[0].str.lower() == 'r')]['Icon URL'][:20],
+            (dataframe_img[(['FR' in data for data in list(dataframe_img['Languages'].apply(str))]) & (dataframe_img['Name'].str[0].str.lower() == 'r')]['Icon URL'][:40],
             'Juego de la app store con lenguaje frances que su nombre empieza con r'), 
             
             # ----- viernes con texto -----
-            (dataframe_pal[['robot' in data for data in list(dataframe_pal['description'])]]['title'][:20]
+            (dataframe_pal[['robot' in data for data in list(dataframe_pal['description'])]]['title'][:40]
             , 'Peliculas o series de netflix cuya descripcion contiene la palabra robot')
         )
     ]
